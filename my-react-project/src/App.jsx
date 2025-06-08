35322c59f42b1af708d46cdd393db77942bee74a
patrick <habiyaremyepatrick.hp@gmail.com>
2025-06-08 20:00:07 +0200

    todo

--- my-react-project/src/App.jsx
+++ my-react-project/src/App.jsx
@@ -5,7 +5,7 @@ function App(){
     <form className="form">
       <div className="form-group">
         <label htmlfor="todo-input">New Item</label>
-        <input type="text" id="inputs" />
+        <input type="text" id="inputs" placeholder="Add a new Item" />
       </div>
       <button className="add-button">Add</button>
     </form>
@@ -25,8 +25,18 @@ function App(){
         <button className="delete-button">Delete</button>
       </li>
     </ul>
+    <p>Comment:</p>
+    <select>
+      <option value=""></option>
+      <option value="Visa">Visa</option>
+      <option value="Mastercard">Mastercard</option>
+      <option value="momo">Momo Pay</option>
+      <option value="Airtel">Airtel</option>
+      <option value="PayPal">PayPal</option>
+    </select>
     </>
   );
 }
 
 export default App
+e
